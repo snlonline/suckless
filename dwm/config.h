0d4f078284c5e6e83fc34f77c9133dbba6d8bbef
Sanele Mthembu <80328015+snlonline@users.noreply.github.com>
2023-12-21 12:24:13 +0200

    Update config.h
    original numbers

--- dwm/config.h
+++ dwm/config.h
@@ -37,7 +37,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
